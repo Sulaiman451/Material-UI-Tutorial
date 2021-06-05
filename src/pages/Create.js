92388f83f0844cb2df62c0caab98505d6b1e3f4a
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-06-05 15:45:00 +0100

    Use the icon component for demo purposes

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-// import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import SendIcon from "@material-ui/icons/Send";
+import AcUnitOutlinedIcon from "@material-ui/icons/AcUnitOutlined";
 
 export default function Create() {
   return (
-    <Container>
+    <Container size="sm">
       <Typography
         variant="h6"
         color="textSecondary"
@@ -21,22 +23,20 @@ export default function Create() {
         type="submit"
         color="secondary"
         variant="contained"
+        // startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
 
-      {/* <Button type="submit" color="primary">
-        Submit
-      </Button>
-      <Button type="submit" color="secondary" varaint="outline">
-        Submit
-      </Button> */}
-
-      {/* <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
+      {/* icons */}
+      <br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+      <AcUnitOutlinedIcon color="action" fontSize="small" />
+      <AcUnitOutlinedIcon color="error" fontSize="small" />
+      <AcUnitOutlinedIcon color="disabled" fontSize="small" />
     </Container>
   );
 }
