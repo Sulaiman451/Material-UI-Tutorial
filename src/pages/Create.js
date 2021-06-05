83e7fe9880d025252cfb33092652f95471a534c7
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-06-05 15:35:05 +0100

    Look at button component and its variations

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+// import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Container from "@material-ui/core/Container";
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         color="textSecondary"
@@ -12,6 +15,28 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit" color="primary">
+        Submit
+      </Button>
+      <Button type="submit" color="secondary" varaint="outline">
+        Submit
+      </Button> */}
+
+      {/* <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
