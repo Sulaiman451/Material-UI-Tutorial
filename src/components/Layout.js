4588eaa6cfa5f07f007f322765e8ced09cd6118c
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-06-06 17:24:35 +0100

    Add permanent side drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,23 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     backgroundColor: "#f9f9f9",
     width: "100%",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
   },
 });
 
@@ -12,10 +25,20 @@ export default function Layout({ children }) {
   const classes = useStyles;
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* App bar */}
 
       {/* Side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">Ninja Notes</Typography>
+        </div>
+      </Drawer>
 
       <div className={classes.page}>{children}</div>
     </div>
